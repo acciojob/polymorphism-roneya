18dcddc8b691becc6c1bdc99745cc13591dd6750
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-01-15 23:47:25 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,15 +9,15 @@ public class Main {
         public int product(int x, int y, int z) {
             return x*y*z;
         }
-        public double product(double x, double y, double z) {
-        return x*y*z;
+        public double product(double x, double y) {
+        return x*y;
         }
     }
     public static void main(String[] args) {
         Product p = new Product();
         System.out.println(p.product(5,7));
         System.out.println(p.product(5,7,9));
-        System.out.println(p.product(5.0,7.0,9.0));
+        System.out.println(p.product(5.0,7.0));
     }
 
 
